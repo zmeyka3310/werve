e1fd58a81ee3b308566543623c4f0ddd5221c0af
zmeyka3310 <castella3310@gmail.com>
2026-04-22 23:02:08 +0300

    user search string is now lowercased

--- src/main.rs
+++ src/main.rs
@@ -178,7 +178,7 @@ fn reeval(searchterm: &str, desktopfiles: Vec<(String, String, String, i32)>) ->
     if searchterm.is_empty() {
         return desktopfiles;
     }
-    let mut desktopfiles2 = desktopfiles.into_iter().map(|entry| reeval_single(searchterm, entry)).collect::<Vec<_>>();
+    let mut desktopfiles2 = desktopfiles.into_iter().map(|entry| reeval_single(searchterm.to_lowercase(), entry)).collect::<Vec<_>>();
     desktopfiles2.sort_by(|a, b| a.3.cmp(&b.3)); // 3 is score, lower means more relevant
     desktopfiles2
 }
